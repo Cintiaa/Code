9907304c2d01de924815a660128e03d829e57402
Cintiaa <cintiasantos@alunos.utfpr.edu.br>
2019-04-09 22:31:41 -0300

    teste fisherface

---
+++ src/fisherface_yale_test.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import os
+from PIL import Image
+
+detectorFacial = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+reconhecedorFacial = cv2.face.FisherFaceRecognizer_create()
+reconhecedorFacial.read('classificador/FisherYale.yml')
+
+totalAcertos = 0
+percentalAcertos = 0.0
+totalConfianca = 0.0
+
+caminhos = [os.path.join('databases/yalefaces/teste', f) for f in os.listdir('databases/yalefaces/teste')]
+for caminhoImagem in caminhos:
+    imagemFace = Image.open(caminhoImagem).convert('L')
+    imagemFaceNP = np.array(imagemFace, 'uint8')
+    facesDetectadas = detectorFacial.detectMultiScale(imagemFaceNP)
+
+    for(x, y, l, a) in facesDetectadas:
+        cv2.rectangle(imagemFaceNP, (x, y), (x + l, y + a), (0, 0, 255), 2)
+        cv2.imshow('Faces', imagemFaceNP)
+        cv2.waitKey(1000)
+        idprevisto, confianca = reconhecedorFacial.predict(imagemFaceNP)
+        idatual = int(os.path.split(caminhoImagem)[1].split(".")[0].replace("subject", ""))
+        print('{} foi classificado como se fosse {} - {}'.format(str(idatual), str(idprevisto), str(confianca)))
+
+        if idprevisto == idatual:
+            totalAcertos += 1
+            totalConfianca += confianca
+
+percentalAcertos = (totalAcertos / 30) * 100
+totalConfianca = totalConfianca / totalAcertos
+
+print('Percentual de acerto: ' + str(percentalAcertos) + '%')
+print('Total de confiança: ' + str(confianca))
